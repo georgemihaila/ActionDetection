516ae4cef47fa1299cd46d76e0cca227a4cd1cb0
George Mihăilă <george_mihaila@outlook.com>
2022-05-24 08:43:02 +0300

    Update Camera.cpp

--- cam/Camera.cpp
+++ cam/Camera.cpp
@@ -24,44 +24,45 @@ static const char *_STREAM_CONTENT_TYPE = "multipart/x-mixed-replace;boundary=" 
 static const char *_STREAM_BOUNDARY = "\r\n--" PART_BOUNDARY "\r\n";
 static const char *_STREAM_PART = "Content-Type: image/jpeg\r\nContent-Length: %u\r\n\r\n";
 
+camera_config_t camConfig;
+
 Camera::Camera()
 {
-    camera_config_t config;
-    config.ledc_channel = LEDC_CHANNEL_0;
-    config.ledc_timer = LEDC_TIMER_0;
-    config.pin_d0 = Y2_GPIO_NUM;
-    config.pin_d1 = Y3_GPIO_NUM;
-    config.pin_d2 = Y4_GPIO_NUM;
-    config.pin_d3 = Y5_GPIO_NUM;
-    config.pin_d4 = Y6_GPIO_NUM;
-    config.pin_d5 = Y7_GPIO_NUM;
-    config.pin_d6 = Y8_GPIO_NUM;
-    config.pin_d7 = Y9_GPIO_NUM;
-    config.pin_xclk = XCLK_GPIO_NUM;
-    config.pin_pclk = PCLK_GPIO_NUM;
-    config.pin_vsync = VSYNC_GPIO_NUM;
-    config.pin_href = HREF_GPIO_NUM;
-    config.pin_sscb_sda = SIOD_GPIO_NUM;
-    config.pin_sscb_scl = SIOC_GPIO_NUM;
-    config.pin_pwdn = PWDN_GPIO_NUM;
-    config.pin_reset = RESET_GPIO_NUM;
-    config.xclk_freq_hz = 20000000;
-    config.pixel_format = PIXFORMAT_JPEG;
+    camConfig.ledc_channel = LEDC_CHANNEL_0;
+    camConfig.ledc_timer = LEDC_TIMER_0;
+    camConfig.pin_d0 = Y2_GPIO_NUM;
+    camConfig.pin_d1 = Y3_GPIO_NUM;
+    camConfig.pin_d2 = Y4_GPIO_NUM;
+    camConfig.pin_d3 = Y5_GPIO_NUM;
+    camConfig.pin_d4 = Y6_GPIO_NUM;
+    camConfig.pin_d5 = Y7_GPIO_NUM;
+    camConfig.pin_d6 = Y8_GPIO_NUM;
+    camConfig.pin_d7 = Y9_GPIO_NUM;
+    camConfig.pin_xclk = XCLK_GPIO_NUM;
+    camConfig.pin_pclk = PCLK_GPIO_NUM;
+    camConfig.pin_vsync = VSYNC_GPIO_NUM;
+    camConfig.pin_href = HREF_GPIO_NUM;
+    camConfig.pin_sscb_sda = SIOD_GPIO_NUM;
+    camConfig.pin_sscb_scl = SIOC_GPIO_NUM;
+    camConfig.pin_pwdn = PWDN_GPIO_NUM;
+    camConfig.pin_reset = RESET_GPIO_NUM;
+    camConfig.xclk_freq_hz = 20000000;
+    camConfig.pixel_format = PIXFORMAT_JPEG;
 
     if (psramFound())
     {
-        config.frame_size = FRAMESIZE_UXGA;
-        config.jpeg_quality = 10;
-        config.fb_count = 2;
+        camConfig.frame_size = FRAMESIZE_UXGA;
+        camConfig.jpeg_quality = 10;
+        camConfig.fb_count = 2;
     }
     else
     {
-        config.frame_size = FRAMESIZE_SVGA;
-        config.jpeg_quality = 12;
-        config.fb_count = 1;
+        camConfig.frame_size = FRAMESIZE_SVGA;
+        camConfig.jpeg_quality = 12;
+        camConfig.fb_count = 1;
     }
 
-    esp_err_t err = esp_camera_init(&config);
+    esp_err_t err = esp_camera_init(&camConfig);
     if (err != ESP_OK)
     {
         Serial.printf("Camera init failed with error 0x%x", err);
@@ -76,7 +77,12 @@ void Camera::respondWithFrame(WebServer *server, framesize_t resolution)
     uint8_t *_jpg_buf = NULL;
     char *part_buf[64];
 
-    //server->sendContent(_STREAM_CONTENT_TYPE);
+    if (camConfig.frame_size != resolution)
+    {
+        esp_camera_deinit();
+        camConfig.frame_size = resolution;
+        esp_camera_init(&camConfig);
+    }
     fb = esp_camera_fb_get();
     if (!fb)
     {
@@ -105,7 +111,7 @@ void Camera::respondWithFrame(WebServer *server, framesize_t resolution)
 
     // size_t hlen = snprintf((char *)part_buf, 64, _STREAM_PART, _jpg_buf_len);
     // server->sendContent((const char *)part_buf, hlen);
-    //server->sendHeader("Content-Type", "image/jpeg");
+    // server->sendHeader("Content-Type", "image/jpeg");
     server->sendContent((const char *)_jpg_buf, _jpg_buf_len);
     if (fb)
     {
