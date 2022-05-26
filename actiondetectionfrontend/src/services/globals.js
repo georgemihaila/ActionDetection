bed31daa79c36dc704a1b60a0ebae6826595b755
George Mihăilă <george_mihaila@outlook.com>
2022-05-26 10:51:02 +0300

    Update globals.js

--- actiondetectionfrontend/src/services/globals.js
+++ actiondetectionfrontend/src/services/globals.js
@@ -1,6 +1,6 @@
 import ApiClient from './api-gen/src/ApiClient'
 import CameraApi from './api-gen/src/api/CameraApi'
 
-var client = new ApiClient("http://localhost:5219");
+var client = new ApiClient("http://10.10.0.157:5219");
 var globalCameraAPI = new CameraApi(client);
 export default globalCameraAPI;
